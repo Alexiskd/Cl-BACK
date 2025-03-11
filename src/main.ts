d0e5194780c47a96c0b2600c4fe94dfd9f2770e0
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-11 14:36:46 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
     process.env.CORS_ORIGIN || 'http://localhost:5173',
     'https://frontend-f4rf.onrender.com',
     'https://frontend-f4rf.onrender.com/',
+    'https://frontend-f4rf.onrender.com',
     'https://cleservice.com/',
     'https://cl-front.onrender.com',
     'https://cl-front.onrender.com/',
