6f97d3e7fff83b6ed8c81eef566fa08a43af6d84
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-01 15:51:20 +0200

    Update mail.config.ts

--- src/mailer/mail.config.ts
+++ src/mailer/mail.config.ts
@@ -3,13 +3,13 @@ export default () => {
   return {
     mail: {
       // Utilise le host défini en variable d'environnement
-      // sauf si celui-ci est "mxplan-vrb1lwo-1", auquel cas on utilise le fallback 'ssl0.ovh.net'
-      host: host && host !== 'mxplan-vrb1lwo-1' ? host : 'ssl0.ovh.net',
+      // sauf si celui-ci est "mxplan-vrb1lwo-1", auquel cas on utilise le fallback 'webmail.mail.ovh.net'
+      host: host && host !== 'mxplan-vrb1lwo-1' ? host : 'webmail.mail.ovh.net',
       port: parseInt(process.env.SMTP_PORT, 10) || 587,
       secure: process.env.SMTP_SECURE === 'true',
-      user: process.env.SMTP_USER || 'Servicecle@cleservice.com',
+      user: process.env.SMTP_USER || 'cleserviceClient@cleservice.com',
       pass: process.env.SMTP_PASS || 'Eliseo3009@',
-      from: process.env.EMAIL_FROM || 'Servicecle@cleservice.com',
+      from: process.env.EMAIL_FROM || 'cleserviceClient@cleservice.com',
     },
   };
 };
