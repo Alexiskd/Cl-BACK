e1ed7deb0d14f84f565cc90682ac1c0cef88f26d
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-08 16:05:24 +0200

    Update produit.controller.ts

--- src/produit/produit.controller.ts
+++ src/produit/produit.controller.ts
@@ -1,4 +1,4 @@
-import { 
+import {
   Controller,
   Get,
   Query,
@@ -70,6 +70,7 @@ export class ProduitController {
       besoinPhoto: newKey.besoinPhoto ?? false,
       besoinNumeroCle: newKey.besoinNumeroCle ?? false,
       besoinNumeroCarte: newKey.besoinNumeroCarte ?? false,
+      fraisDeDossier: newKey.fraisDeDossier ?? 0, // Valeur par défaut à 0
     };
     this.logger.log(`Requête POST reçue pour ajouter la clé: ${JSON.stringify(keyToAdd)}`);
     return this.produitService.addKey(keyToAdd);
@@ -95,6 +96,7 @@ export class ProduitController {
       besoinPhoto: newKey.besoinPhoto ?? false,
       besoinNumeroCle: newKey.besoinNumeroCle ?? false,
       besoinNumeroCarte: newKey.besoinNumeroCarte ?? false,
+      fraisDeDossier: newKey.fraisDeDossier ?? 0, // Valeur par défaut à 0
     }));
     this.logger.log(`Requête POST reçue pour ajouter ${keysToAdd.length} clés.`);
     return this.produitService.addKeys(keysToAdd);
@@ -125,8 +127,6 @@ export class ProduitController {
     return this.produitService.getKeyByIndex(parseInt(index, 10));
   }
 
-  // ------------------ Nouvelles routes pour la gestion par marque ------------------
-
   // Retourne le nombre de clés pour une marque donnée
   @Get('cles/brand/:brand/count')
   async countKeysByBrand(@Param('brand') brand: string): Promise<{ count: number }> {
@@ -153,4 +153,3 @@ export class ProduitController {
     return { message: `Clé avec le nom "${nom}" a été supprimée avec succès.` };
   }
 }
-
