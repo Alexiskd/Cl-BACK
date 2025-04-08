757cb465c38b6b473793d8ee30b93d67cdd21a3b
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-08 16:55:24 +0200

    Update mail.module.ts

--- src/mailer/mail.module.ts
+++ src/mailer/mail.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
-import { ConfigModule } from '@nestjs/config';
+import { MailService } from './mail.service';
 
 @Module({
-  imports: [ConfigModule],
-  providers: [MailService],
   controllers: [MailController],
-  exports: [MailService],
+  providers: [MailService],
 })
 export class MailModule {}
