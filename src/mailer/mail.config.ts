c6a94c145a20db91a01071cd45b49b3507ec8cd0
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-09 10:19:37 +0200

    Update mail.config.ts

--- src/mailer/mail.config.ts
+++ src/mailer/mail.config.ts
@@ -1,13 +1,11 @@
-export default () => {
-  const host = process.env.SMTP_HOST || 'ssl0.ovh.net';  // Utilisation de la valeur par défaut si aucune variable n'est définie
-  return {
-    mail: {
-      host: host,
-      port: parseInt(process.env.SMTP_PORT, 10) || 587,
-      secure: process.env.SMTP_SECURE === 'true',
-      user: process.env.SMTP_USER || 'cleserviceClient@cleservice.com',
-      pass: process.env.SMTP_PASS || 'Eliseo3009@',  // Veuillez changer ce mot de passe si nécessaire
-      from: process.env.EMAIL_FROM || 'cleserviceClient@cleservice.com',
-    },
-  };
-};
+// config/mail.config.ts
+export default () => ({
+  mail: {
+    host: process.env.SMTP_HOST || 'ssl0.ovh.net',
+    port: parseInt(process.env.SMTP_PORT, 10) || 587,
+    secure: process.env.SMTP_SECURE === 'true',
+    user: process.env.SMTP_USER || 'Servicecle@cleservice.com',
+    pass: process.env.SMTP_PASS || 'Eliseo3009@',
+    from: process.env.EMAIL_FROM || 'Servicecle@cleservice.com',
+  },
+});
