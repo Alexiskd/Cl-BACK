4dda5f74038d1534711d85888b8d684c3b39016e
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-10 17:20:21 +0200

    Update produit.controller.ts

--- src/produit/produit.controller.ts
+++ src/produit/produit.controller.ts
@@ -13,31 +13,31 @@ export class ProduitController {
 
   @Get('cles')
   async getKeysByMarque(@Query('marque') marque: string): Promise<CatalogueCle[]> {
-    this.logger.log(`Requête reçue sur /cles avec marque: ${marque}`);
+    this.logger.log(`Requête reçue sur /cles avec marque : ${marque}`);
     return this.produitService.getKeysByMarque(marque);
   }
 
   @Get('cles/by-name')
   async getKeyByName(@Query('nom') nom: string): Promise<CatalogueCle | undefined> {
-    this.logger.log(`Requête reçue sur /cles/by-name avec nom: ${nom}`);
+    this.logger.log(`Requête reçue sur /cles/by-name avec nom : ${nom}`);
     return this.produitService.getKeyByName(nom);
   }
 
   @Get('cles/best-by-name')
   async bestKeyByName(@Query('nom') nom: string): Promise<CatalogueCle> {
-    this.logger.log(`Requête pour la meilleure correspondance par nom: ${nom}`);
+    this.logger.log(`Requête pour la meilleure correspondance par nom : ${nom}`);
     return this.produitService.findBestKeyByName(nom);
   }
 
   @Put('cles/update')
   async updateKeyByName(@Query('nom') nom: string, @Body() updates: Partial<CreateKeyDto>): Promise<CatalogueCle> {
-    this.logger.log(`Requête PUT reçue pour nom: ${nom}`);
+    this.logger.log(`Requête PUT reçue pour nom : ${nom}`);
     return this.produitService.updateKeyByName(nom, updates);
   }
 
   @Post('cles/add')
   async addKey(@Body() newKey: CreateKeyDto): Promise<CatalogueCle> {
-    this.logger.log(`Requête POST reçue pour ajouter la clé: ${JSON.stringify(newKey)}`);
+    this.logger.log(`Requête POST reçue pour ajouter la clé : ${JSON.stringify(newKey)}`);
     return this.produitService.addKey(newKey as CatalogueCle);
   }
 
@@ -93,18 +93,9 @@ export class ProduitController {
   }
 
   @Get('cles/brand/:brand/index/:index')
-  async getKeyByBrandAndIndex(
-    @Param('brand') brand: string,
-    @Param('index') index: string,
-  ): Promise<CatalogueCle> {
+  async getKeyByBrandAndIndex(@Param('brand') brand: string, @Param('index') index: string): Promise<CatalogueCle> {
     this.logger.log(`Requête GET sur /cles/brand/${brand}/index/${index}`);
     return this.produitService.getKeyByBrandAndIndex(brand, parseInt(index, 10));
   }
 
   @Delete('cles/delete')
-  async deleteKeyByName(@Query('nom') nom: string): Promise<{ message: string }> {
-    this.logger.log(`Requête DELETE reçue pour nom: ${nom}`);
-    await this.produitService.deleteKeyByName(nom);
-    return { message: `Clé avec le nom "${nom}" a été supprimée avec succès.` };
-  }
-}
