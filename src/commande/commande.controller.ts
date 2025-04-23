fc91e6fc8c56a7c0b66b06520467531592da5273
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-23 17:51:01 +0200

    Update commande.controller.ts

--- src/commande/commande.controller.ts
+++ src/commande/commande.controller.ts
@@ -69,6 +69,7 @@ export class CommandeController {
         attestationPropriete: body.attestationPropriete === 'true',
         ville: body.ville || '',
         quantity: body.quantity ? parseInt(body.quantity, 10) : 1,
+        date: body
       };
       const numeroCommande = await this.commandeService.createCommande(data);
       return { numeroCommande };
