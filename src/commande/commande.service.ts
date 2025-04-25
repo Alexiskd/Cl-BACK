a1759d8b0dea3ba41e7908a94f7fa5302eab9ddc
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-25 11:35:04 +0200

    Update commande.service.ts

--- src/commande/commande.service.ts
+++ src/commande/commande.service.ts
@@ -1,3 +1,5 @@
+// src/commande/commande.service.ts
+
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
