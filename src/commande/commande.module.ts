f3767232269104062f925a5d3d70ec9cb9025fd1
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-30 09:39:34 +0200

    Update commande.module.ts

--- src/commande/commande.module.ts
+++ src/commande/commande.module.ts
@@ -1,15 +1,32 @@
-// src/commande/commande.module.ts
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CommandeController } from './commande.controller';
-import { CommandeService } from './commande.service';
-import { CommandeGateway } from './commande.gateway';
-import { Commande } from './commande.entity';
+@WebSocketGateway({ cors: true })
+export class CommandeGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('CommandeGateway');
 
-@Module({
-  imports: [TypeOrmModule.forFeature([Commande])],
-  controllers: [CommandeController],
-  providers: [CommandeService, CommandeGateway],
-})
-export class CommandeModule {}
+  afterInit(server: Server) {
+    this.logger.log('WebSocket initialisé');
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connecté: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client déconnecté: ${client.id}`);
+  }
+
+  emitCommandeUpdate(payload: any) {
+    this.server.emit('commandeUpdate', payload);
+  }
+}
