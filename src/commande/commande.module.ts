93e93ea93efa8e12a1a8590b21a7cfc29d9533c0
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-30 10:29:25 +0200

    Update commande.module.ts

--- src/commande/commande.module.ts
+++ src/commande/commande.module.ts
@@ -1,11 +1,4 @@
-// src/commande/commande.module.ts
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { Commande } from './commande.entity';
 import { CommandeController } from './commande.controller';
-import { CommandeService } from './commande.service';
-import { CommandeGateway } from './commande.gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Commande])],
